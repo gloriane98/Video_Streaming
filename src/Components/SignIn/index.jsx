53f8c3949629c553650d371e46ebf3499840bd06
gloriane98 <glorianek66@gmail.com>
2022-10-04 10:17:50 +0100

    feat: retraite de dependance

--- src/Components/SignIn/index.jsx
+++ src/Components/SignIn/index.jsx
@@ -2,30 +2,10 @@ import React from 'react'
 import './SignIn.css'
 import icons from '../../images/iconsAccueil.png'
 import {Link} from 'react-router-dom'
-import { useEffect } from 'react'
-import {GoogleLogin,GoogleLogout} from 'react-google-login'
-import {gapi} from 'gapi-script'
+
 
 const SignIn = () => {
-  const clientId="111468697943-l21kkds6hnfm4hvttdrj3n0jkdqet414.apps.googleusercontent.com";
-  useEffect(()=>{
-    const start= () =>{
-      gapi.client.init({
-        clientId : clientId,
-        scope: "email"
-      });
-    }
-    gapi.load('client: auth2', start);
-  });
-  const onSuccess = (res)=>{
-    console.log(res)
-  }
-  const onFaillure = (res)=>{
-    console.log(res);
-  }
-  const onLogoutSuccess = () => {
-  console.log('SUCESS LOG OUT');
-};
+ 
   return (
     <>
      <main className='signcontainer'>
@@ -34,18 +14,9 @@ const SignIn = () => {
             <h1>SmartView</h1>
             <Link to='/details' className="started">
               
-            <GoogleLogin 
-              clientId={clientId}
-              buttonText="Connect with Google"
-              onSuccess={onSuccess}
-              onFailure={onFaillure}
-              isSignedIn={true}
-              />
+           
               </Link>
-              <GoogleLogout 
-                clientId={clientId}
-                onLogoutSuccess={onLogoutSuccess}
-              />
+           
         </div>
      </main>
     </>
