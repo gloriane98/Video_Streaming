3a4337616524f35248e5c5902881f967c13acc01
gloriane98 <glorianek66@gmail.com>
2022-11-21 15:45:52 +0100

    back

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,16 +1,11 @@
 import axios from "axios";
 import { auth } from "../firebase";
 
-// const verifyUser= (async ()=>{
-//   const idToken= await auth.currentUser?.getIdToken()
-//   axios.get(import.meta.env.VITE_APP_URL, {
-//     Authorization: `Bearer ${idToken}`,
-//   })
+const verifyUser= (async ()=>{
+  const idToken= await auth.currentUser?.getIdToken()
+  axios.get(import.meta.env.VITE_APP_URL, {
+    Authorization: `Bearer ${idToken}`,
+  })
   
-//   console.log(idToken);
-// })
-const idToken=  auth.currentUser?.getIdToken()
-console.log(idToken)
-axios.get(import.meta.env.VITE_APP_URL, {
-  Authorization: `Bearer ${idToken}`,
+  console.log(idToken);
 })
